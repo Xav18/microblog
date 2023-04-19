23571b6be90f5ea1b607c4e8e85a6ec9de4753c0
Xav18 <xavier.p.davis18@gmail.com>
2023-04-19 11:42:51 +0200

    improved get-odk-posts logging

--- app/odk.py
+++ app/odk.py
@@ -47,7 +47,7 @@ def update_review_state(project_id, form_id, submission_id, review_state):
 
 def odk_post():
     i=0
-    print(str(datetime.now()))
+    print('+\n' + str(datetime.now()))
     user = User.query.filter_by(username='odk').first_or_404()
     form_data = client.submissions.get_table(form_id="mb_post", project_id=6) #submission data
     for submission in form_data.get('value'):
